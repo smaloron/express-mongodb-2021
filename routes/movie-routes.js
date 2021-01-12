4eae269ebf223bce23fe5e64d4119d4d65266373
Sebastien Maloron <contact@smaloron.eu>
2021-01-12 14:18:00 +0100

    framework d'aggrégation

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -30,6 +30,31 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.get('/buckets-runtime', async (req, res) => {
+  const movies = await dbManager.getMovies();
+  const result = await movies
+    .aggregate([
+      {
+        $bucket: {
+          groupBy: '$runtime',
+          boundaries: [0, 60, 90, 120],
+          default: 'autres durées',
+          output: {
+            nb: { $sum: 1 },
+            films: {
+              $push: {
+                title: '$title',
+                duration: '$runtime',
+              },
+            },
+          },
+        },
+      },
+    ])
+    .toArray();
+  res.status(200).json(result);
+});
+
 app.get('/number-by-year', async (req, res) => {
   try {
     const movies = await dbManager.getMovies();
@@ -47,6 +72,80 @@ app.get('/number-by-year', async (req, res) => {
   } catch (err) {
     console.log('error');
     console.status(500).json(err);
+  }
+});
+
+app.get('/duration-by-year', async (req, res) => {
+  try {
+    const movies = await dbManager.getMovies();
+    const result = await movies.aggregate([
+      {
+        $match: {
+          type: 'series',
+        },
+      },
+      {
+        $group: {
+          _id: '$year',
+          averageDuration: { $avg: '$runtime' },
+          nb: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { nb: -1 },
+      },
+    ]);
+    const data = await result.toArray();
+    console.log('result');
+    res.status(200).json(data);
+  } catch (err) {
+    console.log('error');
+    res.status(500).json(err);
+  }
+});
+
+app.get('/number-of-comments-by-film', async (req, res) => {
+  const comments = await dbManager.getCollection('sample_mflix', 'comments');
+  const result = await comments
+    .aggregate([
+      {
+        $group: {
+          _id: '$movie_id',
+          nb: { $sum: 1 },
+        },
+      },
+      { $sort: { nb: -1 } },
+    ])
+    .limit(50)
+    .toArray();
+
+  res.status(200).json(result);
+});
+
+app.get('/comment/:id', async (req, res) => {
+  try {
+    const comments = await dbManager.getCollection('sample_mflix', 'comments');
+    const result = await comments
+      .aggregate([
+        {
+          $match: {
+            movie_id: new dbManager.ObjectID(req.params.id),
+          },
+        },
+        {
+          $lookup: {
+            from: 'movies',
+            localField: 'movie_id',
+            foreignField: '_id',
+            as: 'movieDetails',
+          },
+        },
+      ])
+      .limit(50)
+      .toArray();
+    res.json(result);
+  } catch (err) {
+    res.json(err);
   }
 });
 
