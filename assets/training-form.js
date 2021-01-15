d5769f86522ad7879b514b00edcd6f78c0b8c68a
Sebastien Maloron <contact@smaloron.eu>
2021-01-15 12:21:45 +0100

    Correction TP

--- assets/training-form.js
+++ assets/training-form.js
@@ -1,12 +1,28 @@
 $(document).ready(function () {
   // Cibles du DOM
-  const $template = $('#skillTemplate').clone().removeAttr('id');
+  const $skillTemplate = $('#skillTemplate').clone().removeAttr('id');
+  const $traineeTemplate = $('#traineeTemplate').clone().removeAttr('id');
   const $skillsContainer = $('#skillsContainer');
+  const $traineeContainer = $('#traineesContainer');
   const $btAddSkill = $('#btAddSkill');
+
+  $skillTemplate.find('input').val('');
+  $traineeTemplate.find('input').val('');
 
   // Ajout d'une nouvelle compétence
   // au clic sur le bouton
   $btAddSkill.click(function () {
-    $skillsContainer.append($template.clone());
+    $skillsContainer.append($skillTemplate.clone());
+  });
+
+  $('#btAddTrainee').click(function () {
+    //alert('test');
+    console.log($traineeTemplate);
+    console.log($traineeContainer);
+    $traineeContainer.append($traineeTemplate.clone());
+  });
+
+  $('body').delegate('.delete', 'click', function () {
+    $(this).parent().parent().remove();
   });
 });
--- routes/web-training-routes.js
+++ routes/web-training-routes.js
@@ -7,11 +7,57 @@ router.get('/form', (req, res) => {
 });
 
 router.post('/form', async (req, res) => {
+  console.log(req.body.trainees);
   try {
-    const training = new Training(req.body);
+    let training = {};
+
+    console.log(req.body);
+
+    const newTrainees = [];
+    const data = req.body.trainees[0] || [];
+    if (data.length > 1) {
+      for (val of data.name) {
+        newTrainees.push({ name: val });
+      }
+
+      for (index in data.firstName) {
+        newTrainees[index].firstName = data.firstName[index];
+      }
+
+      for (index in data.birthDate) {
+        newTrainees[index].birthDate = new Date(data.birthDate[index]);
+      }
+    } else {
+      newTrainees.push(data);
+      console.log(data);
+    }
+
+    req.body.trainees = newTrainees;
+    delete req.body.trainees;
+
+    console.log(newTrainees);
+
+    if ('_id' in req.body) {
+      console.log('update');
+      training = await Training.findById(req.body._id);
+      for (key in req.body) {
+        training[key] = req.body[key];
+      }
+      training.trainees = newTrainees;
+    } else {
+      console.log('save');
+      //console.log(req.body);
+
+      training = new Training(req.body);
+      training.trainees = newTrainees;
+      //console.log(training);
+    }
+    //res.json({ training, newTrainees });
     await training.save();
     res.redirect('/formation');
   } catch (err) {
+    console.log('err');
+    //console.log(req.body);
     res.status(500).json(err);
   }
 });
@@ -19,6 +65,17 @@ router.post('/form', async (req, res) => {
 router.get('/', async (req, res) => {
   const trainingList = await Training.find();
   res.render('training/home', { trainingList });
+});
+
+router.get('/delete/:id', async (req, res) => {
+  await Training.findByIdAndDelete(req.params.id);
+  res.redirect('/formation');
+});
+
+router.get('/form/:id', async (req, res) => {
+  const training = await Training.findById(req.params.id);
+  console.log(training);
+  res.render('training/form', { training: training });
 });
 
 module.exports = router;
